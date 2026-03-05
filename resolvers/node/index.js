bfeb816e5044d4132f71708dd9ceac200bfa66d8
Jordan Harband <ljharb@gmail.com>
2026-03-05 11:29:44 -0500

    [resolvers] [node] [deps] update `is-core-module`, `resolve`

--- resolvers/node/index.js
+++ resolvers/node/index.js
@@ -55,7 +55,7 @@ exports.resolve = function (source, file, config) {
   }
 
   try {
-    const cachedFilter = function (pkg, dir) { return packageFilter(pkg, dir, config); };
+    const cachedFilter = function (pkg, _pkgFile, dir) { return packageFilter(pkg, dir, config); };
     resolvedPath = resolve(source, opts(file, config, cachedFilter));
     log('Resolved to:', resolvedPath);
     return { found: true, path: resolvedPath };
